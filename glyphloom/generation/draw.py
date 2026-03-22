64168ce9be7af4cc2ef48efa08f2ef8d6881598c
ResoluteVinculum <trentoberlander@gmail.com>
2026-03-22 10:14:26 -0700

    trying out some options

--- glyphloom/generation/draw.py
+++ glyphloom/generation/draw.py
@@ -75,8 +75,6 @@ if __name__ == '__main__':
     leylines = geometry.Leylines(
         founts=geometry.Founts(
             n_points=13,
-            expression=(
-                'array([0,1,2,3,4,5,6,7,8,9,10,11,12])', 
-                'random.randint(0,25,13)*domain')),
+            expression=None),
         expression='linear')
     Glyph(spelldata, leylines).draw()
